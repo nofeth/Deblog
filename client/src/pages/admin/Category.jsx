b9fa5e22b17ef9b2986f7917ac5f5dc9276f7722
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2022-10-07 22:12:55 +0700

    add

--- client/src/pages/admin/Category.jsx
+++ client/src/pages/admin/Category.jsx
@@ -8,6 +8,7 @@ const CategoryAdmin = () => {
     const [title,setTitle] = useState('')
     const [data,setData] = useState([])
     const [message,setMessage] = useState('')
+    const [edit,setEdit] = useState(false)
     const [editSelected,setEditSelected] = useState([])
     async function handleForm(e){
         e.preventDefault()
@@ -16,21 +17,32 @@ const CategoryAdmin = () => {
             getData()
             setTitle('')
             
-        } catch (error) {
-            console.log("error");
+        } catch ({response : {data}}) {
+            console.log(data);
         }
     }
 
-    function handleFormEdit(element){
-        
-       const filter = editSelected.includes(element) // false
-       if (!filter) {
-           setEditSelected([...editSelected,element])
-           return
+    async function handleFormDelete(){
+        try {
+            await axios.delete(`${api}/categories/`,{
+               data : {data : editSelected}
+            })
+            getData()
+        } catch (error) {
+            
         }
-        const remove = editSelected.filter(e => e !== element)
-        setEditSelected(remove)
-        return 
+    }
+
+    function handleFormEdit({ _id : id = 0,title = 0}){
+           const filter = editSelected.map(e => e.title).includes(title) // false
+           if (!filter) {
+               setEditSelected([...editSelected,{id,title}])
+               setEdit(true)
+               return
+            }  
+            const remove = editSelected.filter(e => e.title !== title)
+            setEditSelected(remove)
+            return 
    }
     async function getData(){
         try {
@@ -54,13 +66,13 @@ const CategoryAdmin = () => {
             <div className=" border-t-[10px] basis-full border-[#395B64] p-5 shadow-lg rounded-lg">
             <div className='flex text-2xl mb-5 mx-5 justify-end items-center gap-x-5'>
                 <button className={`${editSelected.length > 0 ? 'text-[rgb(0,0,0,1)]' : 'text-[rgb(0,0,0,.5)]'} hover:text-black transition p-2 rounded-full shadow-md`}><MdModeEdit/></button>
-                <button onClick={handleFormEdit} className={`${editSelected.length > 0 ? 'text-[rgb(0,0,0,1)]' : 'text-[rgb(0,0,0,.5)]'} text-[rgb(0,0,0,.5)] hover:text-black transition p-2 rounded-full shadow-md`}><MdDelete/></button>
+                <button onClick={handleFormDelete} className={`${editSelected.length > 0 ? 'text-[rgb(0,0,0,1)]' : 'text-[rgb(0,0,0,.5)]'} text-[rgb(0,0,0,.5)] hover:text-black transition p-2 rounded-full shadow-md`}><MdDelete/></button>
             </div>
                 <article className="max-h-[500px] overflow-y-scroll flex gap-1 items-start flex-wrap justify-start">
                     {
                         data.map(e => {
                             return (
-                                <span key={e._id} onClick={() => handleFormEdit(e.title)} className={`${editSelected.includes(e.title) ? 'bg-[#395B64] border-[#395B64] text-slate-50' : 'bg-white'} cursor-pointer hover:bg-[#395B64] hover:text-slate-50 hover:border-[#395B64] transition p-2 border rounded-lg`}>{e.title}</span>
+                                <span key={e._id} onClick={() => handleFormEdit(e)} className={`${editSelected.map(e => e.title).includes(e.title) ? 'bg-[#395B64] border-[#395B64] text-slate-50' : 'bg-white'} cursor-pointer hover:bg-[#395B64] hover:text-slate-50 hover:border-[#395B64] transition p-2 border rounded-lg`}>{e.title}</span>
                             )
                         })
                     }
@@ -68,7 +80,13 @@ const CategoryAdmin = () => {
             </div>
             <div className=" basis-1/3 ">
                 {
+                    (editSelected.length > 0) ?
+                    editSelected.map(e => {
+                        return <FormCategory title={title} setTitle={(e) => setTitle(e.target.value)} message={message} handleForm={handleForm}/>
+                    })
+                    :
                     <FormCategory title={title} setTitle={(e) => setTitle(e.target.value)} message={message} handleForm={handleForm}/>
+
                 }
             </div>
                 
