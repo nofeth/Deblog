d2c828d03e84f9afb4ab59d3e6f96d4d25631e72
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2022-10-28 22:44:07 +0700

    add

--- client/src/pages/Article.jsx
+++ client/src/pages/Article.jsx
@@ -1,16 +1,33 @@
+import { useState, useEffect } from "react"
+import axios from 'axios'
 import { IoArrowForwardSharp } from "react-icons/io5"
 import TitleSection from "../components/atoms/TitleSection"
-import CardContent from "../components/CardsContent"
-
+import CardContent from "../components/CardContent"
+import api from '../api/api'
 const Article = () => {
+    const [data,setData] = useState([])
+
+    async function getData(){
+        try {
+            const {data : {data}} = await axios.get(`${api}/article`)
+            setData(JSON.parse(data))
+        } catch (error) {
+            console.log(error.message);
+        }
+    }
+    useEffect(() => {
+      getData()
+
+    }, [])
+    
     return (
        <>
         <section className="container font-playfair">
             <TitleSection style="my-10 text-center" text="Artikel"/>
             <article className="flex flex-wrap">
-                  <CardContent/>
-                  <CardContent/>
-                  <CardContent/>
+            {data.map(e => {
+                    return <CardContent key={e._id} author={e.author} content={e.content} date={e.date} links={e._id}/>
+                })}
             </article>
         </section>
         
