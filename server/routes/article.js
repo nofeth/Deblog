9b81cc48e2599542c39123fb6bf85c4bfd486f70
Ade Oktaviano Arrahman <adeoktaviano50@gmail.com>
2022-10-28 22:43:33 +0700

    add

--- server/routes/article.js
+++ server/routes/article.js
@@ -1,9 +1,10 @@
 const express = require('express')
-const { getAllArticle, addArticle } = require('../controllers/articleControllers.js')
+const { getAllArticle, addArticle, detailArticle } = require('../controllers/articleControllers.js')
 
 const router = express.Router()
 
 router.get('/',getAllArticle)
+router.get('/:id',detailArticle)
 
 router.post('/',addArticle)
 
